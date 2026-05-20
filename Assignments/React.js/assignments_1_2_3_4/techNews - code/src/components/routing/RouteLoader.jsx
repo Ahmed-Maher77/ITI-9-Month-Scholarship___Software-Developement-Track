0ec7411567721b6106e07de0c8777b1fa2dd7056
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-05-21 02:32:15 +0300

    feat: add RoleRoutes, RouteLoader, useDebounce hook, and ModerationQueue component for improved routing and loading states

---
+++ Assignments/React.js/assignments_1_2_3_4/techNews - code/src/components/routing/RouteLoader.jsx
@@ -0,0 +1,11 @@
+import "./RouteLoader.css";
+
+function RouteLoader() {
+    return (
+        <div className="route-loader-wrap">
+            <div className="loader"></div>
+        </div>
+    );
+}
+
+export default RouteLoader;
